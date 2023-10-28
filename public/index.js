47e4b6c43bfafb0288e8eae0adc68ebb4457bea6
Denys <go4denys@gmail.com>
2023-10-28 16:08:31 -0400

    add Links of the navbar section

---
+++ public/index.js
@@ -0,0 +1,25 @@
+import ellipse from './Ellipse.svg';
+import etsy from "./etsy.svg"
+import facebook from "./facebook.svg";
+import image1 from "./image1.svg"
+import image2 from "./image2.svg"
+import image3 from "./image3.svg"
+import instagram from "./instagram.svg"
+import lines from './Lines.svg'
+import logo_svg from './logo.svg'
+import logo_png from './logo.png'
+import star from './Star.svg'
+import starGold from './Star_gold.svg'
+import line from './Line_1.svg'
+import search from "./search.svg"
+import testimonial_background from './testmonial_background.svg'
+import nav_icon from './nav-list-icon.png'
+import wood_hammer from './wood_hammer.jpg'
+import hammer_wood_product from './hammer_wood_product.jpg'
+import cheese_board from './cheese_board.jpg'
+import wiskey_smoker from './wiskey_smoker.jpg'
+
+
+export { ellipse, etsy, facebook, image1, image2, image3, instagram, lines, logo_svg, star, starGold, line, logo_png, search, testimonial_background, nav_icon, wood_hammer, hammer_wood_product, cheese_board, wiskey_smoker }
+
+
