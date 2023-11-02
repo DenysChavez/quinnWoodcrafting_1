c37e2a849076f5a144446468952bf36139b09163
Denys <go4denys@gmail.com>
2023-11-02 19:05:58 -0400

    add aboutPage content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
 
 function page() {
   return (
-    <div>About Us</div>
-  )
+    <div className="mt-16 regular-20">
+      <section className="max-container padding-container flex flex-col gap-5 py-10 text-center justify-center">
+        <h1 className="regular-40">About Us</h1>
+        <hr />
+        <p>
+          Welcome to Quinn Woodcrafting, the essence of craftsmanship where
+          every grain tells a story.
+        </p>
+        <p>
+          Founded by brothers Dan and Kevin Quinn, our workshop is a haven where
+          the timeless beauty of wood meets the ingenuity of modern design. We
+          grew up surrounded by the whispering forests of our hometown, inspired
+          by the intricate dance of nature and the art it could produce in
+          skilled hands.
+        </p>
+        <p>
+          We delight in the challenge of custom projects, relishing the
+          opportunity to bring your unique visions to fruition. Each custom
+          piece is a collaboration, a journey we undertake with you, ensuring
+          that the final product isn't just made to order, but made to inspire
+        </p>
+        <p>
+          At Quinn Woodcrafting, we are committed to sustainable practices,
+          honoring the environment that fuels our creativity. We hand-select the
+          finest materials, championing local wood sourced with respect for our
+          forests, ensuring that we're not just making pieces for today, but for
+          tomorrow as well.
+        </p>
+        <p>
+          So, whether you're looking to elevate your home with a conversation
+          piece, searching for that perfect, personalized gift, or simply
+          sharing our love for all things wood, we invite you to explore our
+          collection. Discover the warmth, the resilience, and the character of
+          wood as envisioned by Quinn Woodcrafting.
+        </p>
+        <p>
+          Thank you for taking the time to visit us. We're not just crafting
+          wood; we're crafting memories, legacy, and love. Welcome to the
+          family.
+        </p>
+        <p>
+          <span>Kevin & Dan Quinn</span>
+          <hr />
+          <span>Quinn Woodcrafting</span>
+        </p>
+      </section>
+    </div>
+  );
 }
 
-export default page
+export default page;
