ae1a21d4abc9262872a7bfb1d537c5905def126f
Denys <go4denys@gmail.com>
2023-11-02 19:46:49 -0400

    change the fill to true of the Image component in the LatestProducts section for a better resolution of the image

--- components/LatestProducts.tsx
+++ components/LatestProducts.tsx
@@ -22,35 +22,39 @@ function LatestProducts() {
       imageSrc: "/wood_hammer.jpg",
     },
   ];
-  
 
   return (
     <div className="bg-background-10">
-<section className=" bg-background-10 max-container padding-container flex flex-col gap-5 py-10">
-      <h2 className="text-white text-3xl text-center">LATEST PRODUCTS</h2>
-      <div className="grid grid-flow-col scroll-auto gap-[1.2rem] overflow-y-auto overscroll-x-contain scroll-snap-x scrollbar-none carrousel">
-        {products.map((product, index) => (
-          <div
-            key={index}
-            className="flex flex-col items-center justify-between mb-0 product-border min-w-[342px] max-w-[342px]"
-          >
-            <Image
-              src={product.imageSrc}
-              alt={product.name}
-              width={250}
-              height={250}
-              objectFit="cover"
-              className="h-[250px] w-[300px] object-cover block"
-            />
-            <h2 className="regular-24 font-semibold mt-4 mb-2 text-white">{product.name}</h2>
-            <p className="regular-20 font-semibold text-gray-10 w-[80%] mb-4">{product.description}</p>
-            <span className="text-lg font-bold text-gold-10 mb-2">{product.price}</span>
-          </div>
-        ))}
-      </div>
-    </section>
+      <section className=" bg-background-10 max-container padding-container flex flex-col gap-5 py-10">
+        <h2 className="text-white text-3xl text-center">LATEST PRODUCTS</h2>
+        <div className="grid grid-flow-col scroll-auto gap-[1.2rem] overflow-y-auto overscroll-x-contain scroll-snap-x scrollbar-none carrousel">
+          {products.map((product, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center justify-between mb-0 product-border min-w-[342px] max-w-[342px]"
+            >
+              <div className="h-[250px] w-[300px] relative overflow-hidden">
+                <Image
+                  src={product.imageSrc}
+                  alt={product.name}
+                  fill={true}
+                  className="h-[250px] w-[300px] object-cover"
+                />
+              </div>
+              <h2 className="regular-24 font-semibold mt-4 mb-2 text-white">
+                {product.name}
+              </h2>
+              <p className="regular-20 font-semibold text-gray-10 w-[80%] mb-4">
+                {product.description}
+              </p>
+              <span className="text-lg font-bold text-gold-10 mb-2">
+                {product.price}
+              </span>
+            </div>
+          ))}
+        </div>
+      </section>
     </div>
-    
   );
 }
 
