2985bf69cf724d8ae59a988e3d35cb094d8c105e
Denys <go4denys@gmail.com>
2023-11-03 10:21:28 -0400

    add text to the Event page

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,7 +1,50 @@
 const page = () => {
   return (
-    <div>Workshops & Events</div>
-  )
-}
+    <div className="mt-16 regular-20">
+      <section className="max-container padding-container flex flex-col gap-5 py-10 text-center justify-center">
+        <h1 className="regular-40">Events</h1>
+        <p>
+          We dream of opening our doors to a series of events that celebrate the
+          art of woodcrafting. Picture evenings of camaraderie around crafted
+          tables, where the scent of fresh timber blends with laughter and
+          storytelling. Our envisioned events include:
+        </p>
 
-export default page
+        <ul>
+          <li>Local Artisan Showcases</li>
+          <li>Live Craft Demonstrations</li>
+          <li>Community Build Projects</li>
+          <li>Seasonal Craft Fairs</li>
+        </ul>
+        <h2 className="regular-32">Stay Updated</h2>
+
+        <p>
+          Our workshop and event calendar are as much a work in progress as our
+          latest woodcraft piece. We invite you to join our community to stay
+          informed about upcoming workshops and events. Sign up for our
+          newsletter, follow us on social media, or bookmark this page -
+          whatever your choice, be assured that exciting things are on the
+          horizon at Quinn Woodcrafting.
+        </p>
+        <h2 className="regular-32">Get Involved</h2>
+        <p>
+          Have an idea for a workshop or an event? Looking to collaborate? We
+          are all ears! Reach out to us at [contact information]. Your input
+          could help shape the future of Quinn Woodcrafting's community
+          engagements.
+        </p>
+        <p>
+          Until we can welcome you in person, keep crafting and keep dreaming.
+        </p>
+        <p>With anticipation,</p>
+        <p>
+          <span>Kevin & Dan Quinn</span>
+          <hr />
+          <span>Quinn Woodcrafting</span>
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default page;
