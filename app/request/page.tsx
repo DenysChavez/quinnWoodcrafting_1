910f212a997972dae6e2a9ac21cc0d98762b1dad
Denys <go4denys@gmail.com>
2023-11-04 16:38:05 -0400

    add text to the request page

--- app/request/page.tsx
+++ app/request/page.tsx
@@ -1,7 +1,53 @@
 const page = () => {
   return (
-    <div>Request Page</div>
-  )
-}
+    <div className="mt-16 regular-20">
+      <section className="max-container padding-container flex flex-col gap-5 py-10 text-center justify-center">
+        <h1 className="regular-40">Custom Requests</h1>
+        <p>At Quinn Woodcrafting, your imagination is the only limit.</p>
+        <p>
+          Every piece of wood holds a unique story, and we believe in creating
+          custom pieces that add a chapter to yours. Whether you're dreaming of
+          a custom whiskey smoker to enjoy your evenings, a personalized cutting
+          board for your culinary adventures, a guitar holder that is as unique
+          as your music, or a table that's more than a piece of furniture - we
+          are here to bring your vision to life.
+        </p>
+        <h2 className="regular-32">How It Works</h2>
+        <ul>
+          <li>
+            <span className="font-bold">Share Your Vision: </span>
+            Use the form below to tell us about your idea. The more details you
+            can provide, the better we can visualize your project alongside you.
+          </li>
+          <li>
+            <span className="font-bold">Consultation: </span>
+            We'll reach out to you to discuss your project in depth, refine the
+            design, and make sure all your expectations are clear and
+            achievable.
+          </li>
+          <li>
+            <span className="font-bold">Quote and Timeline: </span>
+            Once we've captured the essence of your request, we will provide you
+            with a quote and an estimated timeline for the creation of your
+            piece.
+          </li>
+          <li>
+            <span className="font-bold">Crafting Your Piece: </span>
+            With your approval, we begin the careful and passionate process of
+            crafting your custom woodcraft, keeping you updated at key
+            milestones.
+          </li>
+          <li>
+            <span className="font-bold">Delivery: </span>
+            Upon completion, we'll make arrangements to ensure your piece
+            arrives safely to its new home.
+          </li>
+        </ul>
+        <h2 className="regular-32">Start Your Custom Request</h2>
+      <p>Fill out the form below, and let's embark on the journey of crafting your unique wood piece.</p>
+      </section>
+    </div>
+  );
+};
 
-export default page
+export default page;
