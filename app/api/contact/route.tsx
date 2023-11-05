93d7acd68d60eefe1b2ec4a3344b0d794fb204e8
Denys <go4denys@gmail.com>
2023-11-05 13:43:58 -0500

    add nodemailer library

--- app/api/contact/route.tsx
+++ app/api/contact/route.tsx
@@ -1,8 +1,20 @@
-import { NextResponse } from "next/server";
+import { transpoter, mailOptions } from '../../ui/config/nodemailer'
 
 export const POST = async (req: any, res: any) => {
     const data = await req.json();
-    console.log(data);
+    try {
+        await transpoter.sendMail({
+            ...mailOptions,
+            subject: 'New Request',
+            text: 'This is a test string',
+            html: "<h1>Test title</h1> <p>Somo body</p>"
+        }) 
+
+        return res.status(200).json({success: true})
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json({message: 'Bad Request'})
+    }
     
-    return NextResponse.json({ message: ['hi from contact route'] });
+
 }
